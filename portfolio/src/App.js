f87f4ae37dfeef0485130a18eb3eb29a57ac3f07
Haris Bin Talat <bin.talat900@gmail.com>
2022-03-23 23:20:52 +0500

    header added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import './index.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
 function App() {
   return (
-    <div className="App">
-      <h2>OK</h2>
+    <div>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
